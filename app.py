e37d4375845504b58f17cb3516e69af4578551d5
Dimitrios <d.kanidis7k@gmail.com>
2019-08-09 20:24:01 +0100

    cleanup and flask side made prevelent

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	message = "This message is coming from Flask"
-	return render_template('index.html')
+	return """This message is coming from Flask.
+			Running on {}, {}""".format(socket.gethostname(), request.remote_addr)
+	# return render_template('index.html')
 
 if (__name__ == "__main__"):
     app.run(host='0.0.0.0', port = 5000, debug=True)
